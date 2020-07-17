53d4baa82076080070e8df7d7be01dbc4328da5e
Isaac Waithaka <waithakaisaac2016@gmail.com>
2020-07-17 14:48:41 +0300

    Add names information

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var myBirthday = parseInt(prompt("When is your birthday in the form DD-MM-CCYR?"));
+if ((DD<=0 || DD>31) || (MM<=0 || m>12)) {
+  console.log("Enter valid Day/Month");
+} else{}
+
+var myGender = prompt("What is your Gender?");
+var dayOfWeek = function(((CC/4)-2*CC-1)+((5*YY/4)+((26*(MM+1)/10))+DD)mod7 {
+return Akan Name;
+}
