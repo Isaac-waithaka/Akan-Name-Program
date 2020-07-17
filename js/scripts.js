8397032bef18b7d60712d82bbd18c0430233a8a6
Isaac Waithaka <waithakaisaac2016@gmail.com>
2020-07-17 15:09:46 +0300

    Enter the changes to initial code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
-var myBirthday = parseInt(prompt("When is your birthday in the form DD-MM-CCYR?"));
-if ((DD<=0 || DD>31) || (MM<=0 || m>12)) {
-  console.log("Enter valid Day/Month");
-} else{
-  var myGender = prompt("What is your Gender?");
-}
 var dayOfWeek = function(DD,MM,CCYR) {
 return (((CC/4)-2*CC-1)+((5*YY/4)+((26*(MM+1)/10))+DD)mod7;
 };
+var DD = parseInt(prompt("Enter the Date of the day you were born"));
+if (DD<=0 || DD>31) {
+  console.log("Enter valid Day");}
+    else {
+var MM = parseInt(prompt("Enter the Date of the month you were born"));
+}
+ if (MM<=0 || m>12)  {
+  console.log("Enter valid Month");}
+  else{
+var CCYR = parseInt(prompt("Enter your year of birth"));
+}
+var myGender = prompt("What is your Gender?");
 var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 return Akan Name;
