bf85b9986980fa64eaa218d9fd08050079fa1456
Isaac Waithaka <waithakaisaac2016@gmail.com>
2020-07-17 14:54:38 +0300

    Add initial function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var myBirthday = parseInt(prompt("When is your birthday in the form DD-MM-CCYR?"));
 if ((DD<=0 || DD>31) || (MM<=0 || m>12)) {
   console.log("Enter valid Day/Month");
-} else{}
-
-var myGender = prompt("What is your Gender?");
-var dayOfWeek = function(((CC/4)-2*CC-1)+((5*YY/4)+((26*(MM+1)/10))+DD)mod7 {
+} else{
+  var myGender = prompt("What is your Gender?");
+}
+var dayOfWeek = function(DD,MM,CCYR) {
+return (((CC/4)-2*CC-1)+((5*YY/4)+((26*(MM+1)/10))+DD)mod7;
+};
+var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 return Akan Name;
 }
