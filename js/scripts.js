419df7d6627542a06e950407daf4e06d82e0f93f
Isaac Waithaka <waithakaisaac2016@gmail.com>
2020-07-17 16:20:20 +0300

    Add HTML info

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,11 @@ var MM = parseInt(prompt("Enter the Date of the month you were born"));
  if (MM<=0 || m>12)  {
   console.log("Enter valid Month");}
   else{
-var CCYR = parseInt(prompt("Enter your year of birth"));
+var CCYY = parseInt(prompt("Enter your year of birth"));
 }
+var result = dayOfWeek(((CC/4)-2*CC-1)+((5*YY/4)+((26*(MM+1)/10))+DD)mod7;
 var myGender = prompt("What is your Gender?");
+if (myGender)
 var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 return Akan Name;
