936f37f35d4c3ff976fbb2c1c20f4150f397e689
Isaac Waithaka <waithakaisaac2016@gmail.com>
2020-10-08 00:47:53 +0300

    Add changes to the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,65 @@
-var dayOfWeek = function(DD,MM,CCYR) {
-return (((CC/4)-2*CC-1)+((5*YY/4)+((26*(MM+1)/10))+DD)mod7;
-};
-var DD = parseInt(prompt("Enter the Date of the day you were born"));
-if (DD<=0 || DD>31) {
-  console.log("Enter valid Day");}
-    else {
-var MM = parseInt(prompt("Enter the Date of the month you were born"));
-}
- if (MM<=0 || m>12)  {
-  console.log("Enter valid Month");}
-  else{
-var CCYY = parseInt(prompt("Enter your year of birth"));
-}
-var result = dayOfWeek(((CC/4)-2*CC-1)+((5*YY/4)+((26*(MM+1)/10))+DD)mod7;
+//hide the alert button
+document.getElementById("alert1").style.display = "none";
+document.getElementById("alert2").style.display = "none";
+//declare an array for my names
+var femaleAkanNames = [
+	"Akosua, <img src='https://bit.ly/2JsWQ5t' height='200px'>",
+	"Adwoa, <img src='https://bit.ly/369OsS7' height='200px'>",
+	"Abenaa, <img src='https://bit.ly/2MRihiN' height='200px'>",
+	"Akua, <img src='https://bit.ly/2Poskxu' height='200px'>",
+	"Yaa, <img src='https://bit.ly/2PoBN7T' height='200px'>",
+	"Afua, <img src='https://bit.ly/2pj47xU' height='200px'>",
+	"Ama, <img src='https://bit.ly/348GTcI' height='200px'>"
+];
+var maleAkanNames = [
+	"Kwasi, <img src='https://bit.ly/2omOfdc' height='200px'>",
+	"Kwadwo, <img src='https://bit.ly/31SWwmV' height='200px'>",
+	"Kwabena, <img src='https://bit.ly/2PphxDi' height='200px'>",
+	"Kwaku, <img src='https://bit.ly/3683NCG' height='200px'>",
+	"Yaw, <img src='https://bit.ly/36eVwx0' height='200px'>",
+	"Kofi, <img src='https://bit.ly/2JsXMH1' height='200px'>",
+	"Kwame, <img src='https://bit.ly/2BRxDNG' height='200px'>"
+];
 
-var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var myGender = prompt("What is your Gender?");
-if (myGender=Male) {
-  var myName = maleAkanNames[result]
+var daysWeek = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+//calling functions
+function getAkanName() {
+	var year = parseInt(document.getElementById("year").value);
+	var month = parseInt(document.getElementById("month").value);
+	var day = parseInt(document.getElementById("day").value);
+	var male = document.getElementById("male");
+	var female = document.getElementById("female");
+
+	//validate date
+	if (day <= 0 || day >= 32) {
+		alert(
+			"Be serious, that can't be correct. Console your guardian or parent for correct !!!DATE!!!"
+		);
+	}
+	var day = new Date(year + "/" + month + "/" + day);
+	//storing date in variable date of birth
+	var dateOfBirth = day.getDay();
+	if (male.checked == true) {
+		document.getElementById("alert1").style.display = "block";
+		document.getElementById("span1").innerHTML =
+			"Awesome! You were born on, " + daysWeek[dateOfBirth];
+		document.getElementById("span2").innerHTML =
+			"your Akan Name is " + maleAkanNames[dateOfBirth];
+	} else if (female.checked == true) {
+		document.getElementById("alert1").style.display = "block";
+		document.getElementById("span1").innerHTML = daysWeek[dateOfBirth];
+		document.getElementById("span2").innerHTML = femaleAkanNames[dateOfBirth];
+	}
 }
-else {
-  var myName = femaleAkanNames[result]
+//refresh button the whole site
+function refreshPage() {
+	window.location.reload();
 }
-alert(myName[result]);
